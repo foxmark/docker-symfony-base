526c0912401a48ec33fd2dc20c6d0684801af35c
Foxmark <pawelpasich@yahoo.co.uk>
2025-08-26 21:53:21 +0100

    extending init.sh

--- init.sh
+++ init.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
+
 mkdir app
 
-echo "Do you want create and edit .env file? (y/n): "
-read answer
-
-if [[ "$answer" == "y" || "$answer" == "yes" ]]; then
-    cp .env.example .env
-    nano .env
-fi
-
+cp .env.example .env
+nano .env
+source .env
 
 echo "Do you want create and edit docker-compose.override.yml file? (y/n): "
-read answer
+read q2
 
-if [[ "$answer" == "y" || "$answer" == "yes" ]]; then
+if [[ "$q2" == "y" || "$q2" == "yes" ]]; then
     cp docker-compose.override.example.yml docker-compose.override.yml
     nano docker-compose.override.yml
 fi
@@ -21,19 +17,34 @@ fi
 docker compose up -d
 docker compose exec php symfony check:requirements
 docker compose exec php symfony new . --no-git --version="lts"
-docker compose exec php symfony composer require symfony/orm-pack
 docker compose exec php symfony composer require symfony/monolog-bundle
-docker compose exec php symfony composer require --dev symfony/profiler-pack
-docker compose exec php symfony composer require --dev symfony/debug-bundle 
-docker compose exec php symfony composer require --dev symfony/test-pack
 docker compose exec php symfony composer require --dev symfony/maker-bundle
-docker compose exec php symfony composer require --dev doctrine/doctrine-fixtures-bundle
 
-echo "Do you want create new database for the project? (y/n): "
-read answer
+echo "Do you want install debug and test tools? (y/n): "
+read q3
 
-if [[ "$answer" == "y" || "$answer" == "yes" ]]; then
-    docker compose exec php symfony console doctrine:database:create
+if [[ "$q3" == "y" || "$q3" == "yes" ]]; then
+    docker compose exec php symfony composer require --dev symfony/profiler-pack
+    docker compose exec php symfony composer require --dev symfony/debug-bundle 
+    docker compose exec php symfony composer require --dev symfony/test-pack
 fi
 
+echo "Do you want install doctrine? (y/n): "
+read q4
+
+if [[ "$q4" == "y" || "$q4" == "yes" ]]; then
+    docker compose exec php symfony composer require symfony/orm-pack
+    docker compose exec php symfony composer require --dev doctrine/doctrine-fixtures-bundle
+
+    echo "Do you want create new database for the project? (y/n): "
+    read q5
+
+    if [[ "$q5" == "y" || "$q5" == "yes" ]]; then
+        docker compose exec php symfony console doctrine:database:create
+    fi
+fi
+
+
 docker compose exec php symfony console about
+
+echo "Access your app: http://localhost:$HOST_PORT"
